afedc78f14610d6aa0d18376700afab4f079c31d
dheerajpatel3448w <dheerajdominetar3448@gmail.com>
2026-06-30 18:00:19 +0530

    api

--- Frontend/src/services/types.js
+++ Frontend/src/services/types.js
@@ -15,7 +15,7 @@ export const STORAGE_KEYS = {
 };
 
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+export const API_BASE_URL = import.meta.env.VITE_API_URL
 
 // API Endpoints Mapping
 export const ENDPOINTS = {
